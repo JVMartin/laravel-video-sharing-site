f5cf42767d632d280697d6eff7fe56a257a2e6d8
Jacob Martin <mindnumb@gmail.com>
2017-01-04 19:47:42 -0800

    Gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const elixir = require('laravel-elixir');
-
 require('laravel-elixir-vue-2');
 
 /*
@@ -14,6 +13,8 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+	mix.sass('app.scss');
+	mix.webpack('app.js');
+
+	mix.version(['css/app.css', 'js/app.js']);
 });
