ab13260bf924b9dd0b7b990df61950b5da2e6585
Jacob Martin <mindnumb@gmail.com>
2017-01-04 19:31:09 -0800

    Socialite

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
 		/*
 		 * Package Service Providers...
 		 */
-
-		//
+		Laravel\Socialite\SocialiteServiceProvider::class,
 
 		/*
 		 * Application Service Providers...
@@ -225,6 +224,9 @@ return [
 		'URL' => Illuminate\Support\Facades\URL::class,
 		'Validator' => Illuminate\Support\Facades\Validator::class,
 		'View' => Illuminate\Support\Facades\View::class,
+
+		// Added:
+		'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
 	],
 
