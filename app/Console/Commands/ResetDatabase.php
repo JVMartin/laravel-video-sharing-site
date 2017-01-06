19c88c4d6417d5f2f85a9afc01a1158129734b38
Jacob Martin <mindnumb@gmail.com>
2017-01-06 15:00:57 -0800

    Reset database command

---
+++ app/Console/Commands/ResetDatabase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Schema;
+use Illuminate\Console\Command;
+
+class ResetDatabase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:reset';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Reset the database.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+    	if (Schema::hasTable('migrations')) {
+			$this->call('migrate:rollback');
+		}
+        $this->call('migrate');
+    	$this->call('db:seed');
+    }
+}
