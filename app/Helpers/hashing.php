395a8d8f851eeb95aeb4231f9d29153857c57adf
Jacob Martin <mindnumb@gmail.com>
2017-01-06 15:32:54 -0800

    Helpers

---
+++ app/Helpers/hashing.php
@@ -0,0 +1,25 @@
+<?php
+
+use Hashids\Hashids;
+
+/**
+ * A helper function for encoding Hashids.
+ *
+ * @param string $id The ID to encode.
+ * @return string|null
+ */
+function encodeHash($id)
+{
+    return Hashids::encode($id);
+}
+
+/**
+ * A helper function for decoding Hashids.
+ *
+ * @param string $hash The hash to decode.
+ * @return int|array|null
+ */
+function decodeHash($hash)
+{
+    return Hashids::decode($hash);
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -23,7 +23,5 @@ class User extends Authenticatable
 	 *
 	 * @var array
 	 */
-	protected $hidden = [
-		'password', 'remember_token',
-	];
+	protected $hidden = ['password', 'remember_token'];
 }
