2f34d53f1a69cd9a37edc443cb4a26948c4c405b
Jacob Martin <mindnumb@gmail.com>
2017-01-16 13:55:20 -0800

    Flush application

---
+++ app/Console/Commands/FlushApplication.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Schema;
+use Illuminate\Console\Command;
+
+class FlushApplication extends Command
+{
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'app:flush';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Flush the application (database, files, etc).';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		if (Schema::hasTable('migrations')) {
+			$this->call('migrate:rollback');
+		}
+		$this->call('migrate');
+		$this->call('db:seed');
+		exec(base_path('fix.sh'));
+		exec('git clean -fxd ' . public_path('img/u'));
+		exec('git clean -fxd ' . storage_path());
+	}
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
 	 * @var array
 	 */
 	protected $commands = [
-		\App\Console\Commands\ResetApplication::class
+		\App\Console\Commands\FlushApplication::class
 	];
 
 	/**
