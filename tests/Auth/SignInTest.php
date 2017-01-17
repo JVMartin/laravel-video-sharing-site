78ef8e300b1e5c8a2324da3264394face81294d4
Jacob Martin <mindnumb@gmail.com>
2017-01-16 16:52:30 -0800

    Tablature

--- tests/Auth/SignInTest.php
+++ tests/Auth/SignInTest.php
@@ -9,25 +9,25 @@ class SignInTest extends TestCase
 	public function testSignInMissingFields()
 	{
 		$this->postJson(route('sign-in.email'), [
-			'email' => '',
-			'password' => ''
-		])
-		->assertResponseStatus(422)
-		->seeJson([
-			'email' => [trans('validation.required', ['attribute' => 'email'])],
-			'password' => [trans('validation.required', ['attribute' => 'password'])],
-		]);
+				'email' => '',
+				'password' => ''
+			])
+			->assertResponseStatus(422)
+			->seeJson([
+				'email' => [trans('validation.required', ['attribute' => 'email'])],
+				'password' => [trans('validation.required', ['attribute' => 'password'])],
+			]);
 	}
 
 	public function testSignInInvalidCredentials()
 	{
 		$this->postJson(route('sign-in.email'), [
-			'email' => 'xxxx@xxx.com',
-			'password' => 'xxxxx'
-		])
-		->assertResponseStatus(422)
-		->seeJson([
-			trans('auth.sign-in.failure')
-		]);
+				'email' => 'xxxx@xxx.com',
+				'password' => 'xxxxx'
+			])
+			->assertResponseStatus(422)
+			->seeJson([
+				trans('auth.sign-in.failure')
+			]);
 	}
 }
