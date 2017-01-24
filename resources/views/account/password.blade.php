e21cd187a522abfe3618227ecf8e12cd77ca05fc
Jacob Martin <mindnumb@gmail.com>
2017-01-23 21:46:13 -0800

    Password stuff

--- resources/views/account/password.blade.php
+++ resources/views/account/password.blade.php
@@ -9,12 +9,12 @@
 	<div class="row">
 		<div class="columns medium-6 end">
 			<label for="password">
-				Password
+				New Password
 				<input type="password" name="password" placeholder="Password" autofocus>
 			</label>
 			<label for="password_confirmation">
-				Confirm Password
-				<input type="password" name="password_confirmation" placeholder="Confirm Password">
+				Confirm New Password
+				<input type="password" name="password_confirmation" placeholder="Confirm New Password">
 			</label>
 		</div>
 	</div>
