998347aa5fdac9fedb14801c51211741e15bc332
Jacob Martin <mindnumb@gmail.com>
2017-01-25 15:59:59 -0800

    Factories?

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,9 +15,16 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
 	static $password;
 
 	return [
-		'name' => $faker->name,
+		'first_name' => $faker->firstName,
+		'last_name' => $faker->lastName,
 		'email' => $faker->unique()->safeEmail,
 		'password' => $password ?: $password = bcrypt('secret'),
 		'remember_token' => str_random(10),
+	];
+});
+
+$factory->define(App\Models\User::class, 'social', function (Faker\Generator $faker) {
+	return [
+		'password' => null
 	];
 });
