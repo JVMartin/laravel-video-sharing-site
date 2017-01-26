f98158fd1a08fafa9101dc9dfd250258e826ff10
Jacob Martin <mindnumb@gmail.com>
2017-01-25 17:06:38 -0800

    Whitespace

--- app/Console/Commands/DeleteUnverifiedUsers.php
+++ app/Console/Commands/DeleteUnverifiedUsers.php
@@ -10,46 +10,46 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class DeleteUnverifiedUsers extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'users:delete-unverified';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'users:delete-unverified';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Delete all unverified users older than a certain number of days.';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Delete all unverified users older than a certain number of days.';
 
 	/**
 	 * @var Connection
 	 */
-    protected $db;
+	protected $db;
 
-    public function __construct(Connection $db)
-    {
-        parent::__construct();
+	public function __construct(Connection $db)
+	{
+		parent::__construct();
 
-        $this->db = $db;
-    }
+		$this->db = $db;
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
 		$verbose = $this->getOutput()->isVerbose();
 
-        $days = config('auth.verifications.expire');
-        $olderThan = Carbon::now()->subDays($days);
+		$days = config('auth.verifications.expire');
+		$olderThan = Carbon::now()->subDays($days);
 
-        // Delete all users who have not verified their emails in time.
-        $this->db->table('verifications')->where('created_at', '<', $olderThan)
+		// Delete all users who have not verified their emails in time.
+		$this->db->table('verifications')->where('created_at', '<', $olderThan)
 			->chunk(100, function($verifications) use ($verbose) {
 				foreach ($verifications as $verification) {
 					$user = User::find($verification->user_id);
@@ -62,8 +62,8 @@ class DeleteUnverifiedUsers extends Command
 				}
 			});
 
-        // Delete the verification rows as well.
-        $this->db->table('verifications')->where('created_at', '<', $olderThan)
-	        ->delete();
-    }
+		// Delete the verification rows as well.
+		$this->db->table('verifications')->where('created_at', '<', $olderThan)
+			->delete();
+	}
 }
