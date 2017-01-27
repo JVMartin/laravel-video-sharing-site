fb64000e0e0a89be7e9ddc1b5916ee83428edf9a
Jacob Martin <mindnumb@gmail.com>
2017-01-27 00:09:41 -0800

    .

--- resources/views/video/submit.blade.php
+++ resources/views/video/submit.blade.php
@@ -5,6 +5,11 @@
 @section('content')
 	<section id="videoSubmit">
 		<div class="row">
+			<div class="column small-12">
+				<h1>Submit Video</h1>
+			</div>
+		</div>
+		<div class="row">
 			<div class="columns medium-6 end">
 				<label for="youtube_url" class="{{ ($errors->has('youtube_url')) ? 'is-invalid-label' : '' }}">
 					Youtube URL
