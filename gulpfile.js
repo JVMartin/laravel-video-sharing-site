8bb76cbb10e937e459879356808e16042debfd2d
Jacob Martin <mindnumb@gmail.com>
2017-01-30 14:48:18 -0800

    Revert back to Elixir

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+// Please don't notify me!
+process.env.DISABLE_NOTIFIER = true;
+
+const elixir = require('laravel-elixir');
+require('laravel-elixir-vue-2');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(mix => {
+	mix.sass('app.scss');
+	mix.webpack('app.js');
+
+	mix.version(['css/app.css', 'js/app.js']);
+
+	mix.copy('node_modules/tinymce/skins', 'public/build/js/skins');
+});
