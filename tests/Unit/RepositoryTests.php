ffa433eb32fd6c244143804bf1523cc6128f9984
Jacob Martin <mindnumb@gmail.com>
2017-02-01 18:14:33 -0800

    Repository test

---
+++ tests/Unit/RepositoryTests.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Repositories\UserRepository;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class RepositoryTests extends TestCase
+{
+	public function testRepository()
+	{
+		$repo = app(UserRepository::class);
+		$email = 'jack@test.com';
+
+		$this->assertInstanceOf(UserRepository::class, $repo);
+
+		$this->assertNull($repo->getByEmail($email));
+		$repo->create([
+			'email' => $email
+		]);
+
+		$user = $repo->getByEmail($email);
+		$this->assertEquals($user->email, $email);
+	}
+}
