f38a63e60a74ce8f3ded959830842f019f5682f1
Jacob Martin <mindnumb@gmail.com>
2017-02-01 19:01:58 -0800

    Better testing

--- tests/Unit/RepositoryTest.php
+++ tests/Unit/RepositoryTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use App\Models\User;
 use App\Repositories\UserRepository;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -16,14 +17,18 @@ class RepositoryTest extends TestCase
 
 		$this->assertInstanceOf(UserRepository::class, $repo);
 
+		// Make sure the user does not get returned.
 		$this->assertNull($repo->getByEmail($email));
+
+		// Create the user and ensure that they do get returned.
 		$repo->create([
 			'email' => $email
 		]);
-
 		$user = $repo->getByEmail($email);
+		$this->assertInstanceOf(User::class, $user);
 		$this->assertEquals($user->email, $email);
 
+		// Delete the user and ensure that they do not get returned.
 		$repo->delete($user);
 		$this->assertNull($repo->getByEmail($email));
 	}
