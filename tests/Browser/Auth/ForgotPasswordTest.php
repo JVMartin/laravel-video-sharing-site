5f172f2968ea8d289d38f806a884d4ff4343afff
Jacob Martin <mindnumb@gmail.com>
2017-02-07 14:13:04 -0800

    .

--- tests/Browser/Auth/ForgotPasswordTest.php
+++ tests/Browser/Auth/ForgotPasswordTest.php
@@ -33,39 +33,4 @@ class ForgotPasswordTest extends DuskTestCase
 
 		Mail::assertSent(ResetPasswordLinkEmail::class);
 	}
-
-	public function testSignInMissingFields()
-	{
-		$this->browse(function(Browser $browser) {
-			$browser->visit(new HomePage)
-				->clickLink('Sign In')
-				->press('Sign In')
-				->waitForText(trans('validation.required', ['attribute' => 'email']))
-				->assertSee(trans('validation.required', ['attribute' => 'password']));
-		});
-	}
-
-	public function testSignInInvalidCredentials()
-	{
-		$this->browse(function(Browser $browser) {
-			$browser->visit(new HomePage)
-				->clickLink('Sign In')
-				->type('@sign-in-email', 'xxxx@xxx.com')
-				->type('@sign-in-password', 'xxxxx')
-				->press('Sign In')
-				->waitForText(trans('auth.sign-in.failure'));
-		});
-	}
-
-	public function testSignInValidCredentials()
-	{
-		$this->browse(function(Browser $browser) {
-			$browser->visit(new HomePage)
-				->clickLink('Sign In')
-				->type('@sign-in-email', 'user@test.com')
-				->type('@sign-in-password', 'test12')
-				->press('Sign In')
-				->waitForText(trans('auth.sign-in.success'));
-		});
-	}
 }
