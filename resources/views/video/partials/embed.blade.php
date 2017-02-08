4f50433554b9d130b89635780e0cef3f84ccb0bd
Jacob Martin <mindnumb@gmail.com>
2017-02-08 10:36:16 -0800

    Tags

--- resources/views/video/partials/embed.blade.php
+++ resources/views/video/partials/embed.blade.php
@@ -1,6 +1,6 @@
 @if ($video->embeddable)
 	<div class="responsive-embed">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/{{ $video->youtube_id }}" frameborder="0" allowfullscreen></iframe>
+		<iframe width="420" height="315" src="https://www.youtube.com/embed/{{ $video->youtube_id }}" frameborder="0" allowfullscreen></iframe>
 	</div>
 @else
 	<a href="https://www.youtube.com/watch?v={{ $video->youtube_id }}" target="_blank">
