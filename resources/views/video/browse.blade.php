eb3e292955e1126c025bd1a2da1656169bb537ab
Jacob Martin <mindnumb@gmail.com>
2017-02-08 10:30:33 -0800

    Gotta figure out seeding next

--- resources/views/video/browse.blade.php
+++ resources/views/video/browse.blade.php
@@ -13,22 +13,6 @@
 					</a>
 				</div>
 			@endforeach
-			@foreach ($submissions as $submission)
-				<div class="column column-block">
-					<a class="thumbnail video" href="{{ $submission->url() }}">
-						<h3 title="{{ $submission->title }}">{{ $submission->title }}</h3>
-						<img src="https://img.youtube.com/vi/{{ $submission->video->youtube_id }}/mqdefault.jpg" />
-					</a>
-				</div>
-			@endforeach
-			@foreach ($submissions as $submission)
-				<div class="column column-block">
-					<a class="thumbnail video" href="{{ $submission->url() }}">
-						<h3 title="{{ $submission->title }}">{{ $submission->title }}</h3>
-						<img src="https://img.youtube.com/vi/{{ $submission->video->youtube_id }}/mqdefault.jpg" />
-					</a>
-				</div>
-			@endforeach
 		</div>
 	</section>
 @endsection
