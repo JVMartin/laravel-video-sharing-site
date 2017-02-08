12ae5a6720a359c35c80fce9c71e80483c64b832
Jacob Martin <mindnumb@gmail.com>
2017-02-08 10:28:45 -0800

    .

--- resources/views/video/submit/get-url.blade.php
+++ resources/views/video/submit/get-url.blade.php
@@ -7,7 +7,7 @@
 		<form action="{{ route('video.submit.url.process') }}" method="POST" autocomplete="off">
 			{!! csrf_field() !!}
 			<div class="row">
-				<div class="columns medium-6 medium-offset-3">
+				<div class="columns large-6 large-offset-3 medium-8 medium-offset-2">
 					<h1>Submit Video</h1>
 					<p>First, paste the YouTube URL below.</p>
 					<label for="youtube_url" class="{{ ($errors->has('youtube_url')) ? 'is-invalid-label' : '' }}">
