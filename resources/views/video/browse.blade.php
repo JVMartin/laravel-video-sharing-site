8a55fa42b94befe363aa6df2d8d7bf7cc48b0862
Jacob Martin <mindnumb@gmail.com>
2017-02-08 09:33:57 -0800

    Sassy

--- resources/views/video/browse.blade.php
+++ resources/views/video/browse.blade.php
@@ -12,7 +12,23 @@
 		<div class="row small-up-2 medium-up-3 large-up-4">
 			@foreach ($submissions as $submission)
 				<div class="column column-block">
-					<a class="thumbnail" href="{{ $submission->url() }}">
+					<a class="thumbnail video" href="{{ $submission->url() }}">
+						<h3 title="{{ $submission->title }}">{{ $submission->title }}</h3>
+						<img src="https://img.youtube.com/vi/{{ $submission->video->youtube_id }}/mqdefault.jpg" />
+					</a>
+				</div>
+			@endforeach
+			@foreach ($submissions as $submission)
+				<div class="column column-block">
+					<a class="thumbnail video" href="{{ $submission->url() }}">
+						<h3 title="{{ $submission->title }}">{{ $submission->title }}</h3>
+						<img src="https://img.youtube.com/vi/{{ $submission->video->youtube_id }}/mqdefault.jpg" />
+					</a>
+				</div>
+			@endforeach
+			@foreach ($submissions as $submission)
+				<div class="column column-block">
+					<a class="thumbnail video" href="{{ $submission->url() }}">
 						<h3 title="{{ $submission->title }}">{{ $submission->title }}</h3>
 						<img src="https://img.youtube.com/vi/{{ $submission->video->youtube_id }}/mqdefault.jpg" />
 					</a>
