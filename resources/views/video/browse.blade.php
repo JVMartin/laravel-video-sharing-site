e4fbc1b37393914ee164ceb520d5372754794214
Jacob Martin <mindnumb@gmail.com>
2017-02-12 11:57:11 -0800

    Break points, h2s

--- resources/views/video/browse.blade.php
+++ resources/views/video/browse.blade.php
@@ -8,7 +8,7 @@
 			@foreach ($submissions as $submission)
 				<div class="column column-block">
 					<a class="thumbnail video" href="{{ $submission->url() }}">
-						<h3 title="{{ $submission->title }}">{{ $submission->title }}</h3>
+						<h2 title="{{ $submission->title }}">{{ $submission->title }}</h2>
 						<img src="https://img.youtube.com/vi/{{ $submission->video->youtube_id }}/mqdefault.jpg" />
 					</a>
 				</div>
