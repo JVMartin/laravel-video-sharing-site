2ffa66e49835d15e64f8bfc30457b8eccdce4ef4
Jacob Martin <mindnumb@gmail.com>
2017-02-13 16:08:46 -0800

    Closable messages

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,19 +1,21 @@
 <section id="partialsMessages">
 	@foreach ($errors->getBag('default')->get('messages') as $message)
-		<div class="row">
-			<div class="columns small-12">
-				<div class="callout alert">
-					{{ $message }}
-				</div>
+		<div class="row column">
+			<div class="callout alert" data-closable>
+				{{ $message }}
+				<button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+					<i class="fa fa-close"></i>
+				</button>
 			</div>
 		</div>
 	@endforeach
 	@foreach ($successes->get('messages') as $message)
-		<div class="row">
-			<div class="columns small-12">
-				<div class="callout success">
-					{{ $message }}
-				</div>
+		<div class="row column">
+			<div class="callout success" data-closable>
+				{{ $message }}
+				<button class="close-button" aria-label="Dismiss alert" type="button" data-close>
+					<i class="fa fa-close"></i>
+				</button>
 			</div>
 		</div>
 	@endforeach
