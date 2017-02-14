987b4de3a9e14252cc13bd287d83aea4a5add1f7
Jacob Martin <mindnumb@gmail.com>
2017-02-13 20:40:50 -0800

    More migration

--- database/migrations/2017_02_14_035746_create_comments_table.php
+++ database/migrations/2017_02_14_035746_create_comments_table.php
@@ -19,17 +19,36 @@ class CreateCommentsTable extends Migration
 			$table->integer('parent_id')->unsigned()->nullable();
 			$table->integer('user_id')->unsigned()->nullable();
 			$table->integer('score')->default(1);
+			$table->boolean('deleted')->default(false);
 			$table->text('contents');
 			$table->timestamps();
+
+			$table->index('submission_id');
 
 			$table->foreign('submission_id')
 				->references('id')->on('submissions')
 				->onDelete('cascade');
 			$table->foreign('parent_id')
+				->references('id')->on('comments');
+			$table->foreign('user_id')
+				->references('id')->on('users');
+		});
+
+		Schema::create('comments_votes', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('comment_id')->unsigned();
+			$table->integer('user_id')->unsigned()->nullable();
+			$table->boolean('up')->default(false);
+			$table->boolean('down')->default(false);
+
+			$table->index('comment_id');
+
+			$table->foreign('comment_id')
 				->references('id')->on('comments')
 				->onDelete('cascade');
 			$table->foreign('user_id')
-				->references('id')->on('users');
+				->references('id')->on('users')
+				->onDelete('cascade');
 		});
 	}
 
@@ -40,6 +59,7 @@ class CreateCommentsTable extends Migration
 	 */
 	public function down()
 	{
+		Schema::dropIfExists('comments_ratings');
 		Schema::dropIfExists('comments');
 	}
 }
