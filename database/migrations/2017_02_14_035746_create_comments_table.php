1de118a8eb45ad1c2705eddf83065556b3270310
Jacob Martin <mindnumb@gmail.com>
2017-02-13 20:03:23 -0800

    First crack at comments

---
+++ database/migrations/2017_02_14_035746_create_comments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('submission_id')->unsigned();
+			$table->integer('parent_id')->unsigned()->nullable();
+			$table->integer('user_id')->unsigned()->nullable();
+			$table->text('contents');
+			$table->timestamps();
+
+			$table->foreign('submission_id')
+				->references('id')->on('submissions')
+				->onDelete('cascade');
+			$table->foreign('parent_id')
+				->references('id')->on('comments')
+				->onDelete('cascade');
+			$table->foreign('user_id')
+				->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+}
