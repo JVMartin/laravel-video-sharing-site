b1cde5fd6e6035f5baa1237558072e9616688787
Jacob Martin <mindnumb@gmail.com>
2017-05-14 20:58:10 -0700

    Thumbnails

--- resources/views/users/notifications.blade.php
+++ resources/views/users/notifications.blade.php
@@ -4,30 +4,41 @@
 
 @section('content')
 	<section id="usersNotifications">
-		@foreach ($notifications as $notification)
-			@php($notifiable = $notification->notifiable)
-			<div class="row column">
-					@if ($notifiable instanceof App\Models\Submission)
-						<a class="notification" href="{{ $notifiable->url() }}" target="_blank">
-							@if ($notification->read_at)
-								<i class="fa fa-circle-o"></i>
-							@else
-								<i class="fa fa-circle"></i>
-							@endif
-							@if ($notification->type == 'comments')
-								Your submission "{{ $notifiable->title }}" has {{ $notification->count }} comment{{ $notification->count > 1 ? 's' : '' }}.
-							@endif
-						</a>
-					@endif
-			</div>
-		@endforeach
-		@if ( ! $notifications->count())
+		@if ($notifications->count())
+			@if (Auth::user()->unreadNotifications()->count())
+				<a class="button">
+					Mark all as read
+				</a>
+			@endif
+		@else
 			<div class="row column">
 				<div class="callout default">
 					No notifications.
 				</div>
 			</div>
 		@endif
+		@foreach ($notifications as $notification)
+			@php($notifiable = $notification->notifiable)
+				@if ($notifiable instanceof App\Models\Submission)
+					<a class="notification" href="{{ $notifiable->url() }}" target="_blank">
+						<div class="row">
+							<div class="column small-2">
+								<img src="https://img.youtube.com/vi/{{ $notifiable->video->youtube_id }}/default.jpg" />
+							</div>
+							<div class="column small-10">
+								@if ($notification->read_at)
+									<i class="fa fa-circle-o"></i>
+								@else
+									<i class="fa fa-circle"></i>
+								@endif
+								@if ($notification->type == 'comments')
+									Your submission "{{ $notifiable->title }}" has {{ $notification->count }} comment{{ $notification->count > 1 ? 's' : '' }}.
+								@endif
+							</div>
+						</div>
+					</a>
+				@endif
+		@endforeach
 		<div class="row column text-center">
 			{{ $notifications->links('pagination::foundation') }}
 		</div>
