497a45f7812ce0c34c3353e826338a9c8cefdeca
Jacob Martin <mindnumb@gmail.com>
2017-05-14 19:51:59 -0700

    Wow notifications good job

--- resources/views/users/notifications.blade.php
+++ resources/views/users/notifications.blade.php
@@ -7,14 +7,29 @@
 		@foreach ($notifications as $notification)
 			@php($notifiable = $notification->notifiable)
 			<div class="row column">
-				<div class="notification">
 					@if ($notifiable instanceof App\Models\Submission)
-						@if ($notification->type == 'comments')
-							Your submission "{{ $notifiable->title }}" has {{ $notification->count }} comment{{ $notification->count > 1 ? 's' : '' }}.
-						@endif
+						<a class="notification" href="{{ $notifiable->url() }}" target="_blank">
+							@if ($notification->read_at)
+								<i class="fa fa-circle-o"></i>
+							@else
+								<i class="fa fa-circle"></i>
+							@endif
+							@if ($notification->type == 'comments')
+								Your submission "{{ $notifiable->title }}" has {{ $notification->count }} comment{{ $notification->count > 1 ? 's' : '' }}.
+							@endif
+						</a>
 					@endif
-				</div>
 			</div>
 		@endforeach
+		@if ( ! $notifications->count())
+			<div class="row column">
+				<div class="callout default">
+					No notifications.
+				</div>
+			</div>
+		@endif
+		<div class="row column text-center">
+			{{ $notifications->links('pagination::foundation') }}
+		</div>
 	</section>
 @endsection
