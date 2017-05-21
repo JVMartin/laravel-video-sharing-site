99e6dc5c457c7da41ae061cab77e14f69c2d658e
Jacob Martin <mindnumb@gmail.com>
2017-05-21 13:29:30 -0700

    Handle 404s

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('template')
+
+@section('title', '404')
+
+@section('content')
+	<section id="errors404">
+		<h1>404</h1>
+		<p>The page you are looking for could not be found.</p>
+	</section>
+@endsection
