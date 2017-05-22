8df3c5d50b32ac90f2c129106a400f1e2f750aa5
Jacob Martin <mindnumb@gmail.com>
2017-05-21 22:03:47 -0700

    Working on Envoy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,13 +2,35 @@
 
 @setup
 	$dir = dirname(__DIR__);
+	$releasesPath = $dir . '/releases';
 	$release = date('YmdHis');
 @endsetup
 
 @story('deploy')
 	git
+	composer
+	yarn
+	links
+	switch
 @endstory
 
 @task('git')
-	echo "{{ $dir }}";
+	[ -d {{ $releasesPath }} ] || mkdir {{ $releasesPath }};
+	cd {{ $releasesPath }};
+	git clone seraph:/git/web/onemore.git {{ $release }};
+@endtask
+
+@task('composer')
+	cd {{ $releasesPath }}/{{ $release }}
+	composer install;
+@endtask
+
+@task('yarn')
+	cd {{ $releasesPath }}/{{ $release }}
+	yarn run prod
+@endtask
+
+@task('links')
+	cd {{ $releasesPath }}/{{ $release }}
+	php artisan storage:link
 @endtask
