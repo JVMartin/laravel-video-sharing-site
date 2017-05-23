1d9770e2f53bdd0e250d9e751e4f350400999f8a
Jacob Martin <mindnumb@gmail.com>
2017-05-22 22:17:15 -0700

    Google analytics

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -29,6 +29,14 @@
 			csrfToken: '{{ csrf_token() }}',
 			user: {!! (Auth::check()) ? json_encode(Auth::user()->toArray()) : 'null' !!},
 		};
+
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-99773341-1', 'auto');
+		ga('send', 'pageview');
 	</script>
 	@yield('js')
 	<script src="{{ mix('js/app.js') }}"></script>
